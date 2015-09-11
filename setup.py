391047f9ef7d65b3131a14c20e3e60a556ad4ef3
alexandrdidyk <alexandrdidyk@gmail.com>
2015-09-11 14:06:11 +0300

    setup fix

--- setup.py
+++ setup.py
@@ -1,36 +1,32 @@
 # -*- coding: utf-8 -*-
-#!/usr/bin/env python
+# !/usr/bin/env python
 
+import os
 from setuptools import setup
 
-setup(name='django-log-file-viewer',
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-log-file-viewer',
     version='0.10',
+    include_package_data=True,
     description='Django admin expansion to read/parse file based Django Logging output.',
-    author='Intelie',
+    author='',
     packages=['django_log_file_viewer'],
-    package_data={'django_log_file_viewer': [
-        'templatetags/',
-        'templates/*.html',
-        'testdata/log/test/.gitignore', # To keep this directory (required for farther tests)
-        'testdata/log/testlog.log',
-        'README.rst',
-        'README',
-        ]},
     classifiers=[
-        'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
         'Framework :: Django',
         'Intended Audience :: Developers',
-        'Intended Audience :: System Administrators',
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
-        'Natural Language :: English',
-        'Operating System :: MacOS :: MacOS X',
-        'Operating System :: Microsoft :: Windows',
-        'Operating System :: POSIX',
+        'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
         'Topic :: Internet :: Log Analysis',
         'Topic :: System :: Logging',
         'Topic :: System :: Systems Administration',
         'Topic :: Text Processing',
-        ],
+    ],
 )
