5e6d08de5768b35e5ea45649ad13834149b86018
Alexander Didyk <alexandrdidyk@gmail.com>
2015-11-23 17:46:00 +0200

    urls fix

--- django_log_file_viewer/urls.py
+++ django_log_file_viewer/urls.py
@@ -3,9 +3,9 @@ from views import *
 
 urlpatterns = patterns(
     '',
-    url(r'^$', logfiles_list, name='log-files-list-admin'),
-    url(r'^(?P<logfile_id>\d+)/$', logfile_view,
+    url(r'^logfile/$', logfiles_list, name='log-files-list-admin'),
+    url(r'^logfile/(?P<logfile_id>\d+)/$', logfile_view,
         name='log-file-admin'),
-    url(r'^(?P<logfile_id>\d+)/csv$', logfile_to_csv,
+    url(r'^logfile/(?P<logfile_id>\d+)/csv$', logfile_to_csv,
         name='log-file-to-csv-admin'),
 )
